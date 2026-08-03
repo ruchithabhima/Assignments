de5a5273ba68e8591b565c2002cb38d912423f9d
ruchithabhima <ruchithabhima26@gmail.com>
2026-08-03 15:34:19 +0530

    checked

--- Assignment9/closemenu.js
+++ Assignment9/closemenu.js
@@ -1,38 +1,36 @@
-$(document).ready(function(){
-    $('.navbar-toggler').click(function(){
-        $('#menuIcon').toggleClass('bi-list bi-x');
-    })
-})
+$(document).ready(function () {
+  $(".navbar-toggler").click(function () {
+    $("#menuIcon").toggleClass("bi-list bi-x");
+  });
+});
 
-   function handleSubmit(event) {
-    event.preventDefault();
+function handleSubmit(event) {
+  event.preventDefault();
 
-    let name = document.getElementById("name").value.trim();
-    let email = document.getElementById("email").value.trim();
-    let message = document.getElementById("message").value.trim();
+  let name = document.getElementById("name").value.trim();
+  let email = document.getElementById("email").value.trim();
+  let message = document.getElementById("message").value.trim();
 
-    if (name === "") {
-        alert("Please enter your name");
-        return;
-    }
+  if (name === "") {
+    alert("Please enter your name");
+    return;
+  }
 
-    if (email === "") {
-        alert("Please enter your email");
-        return;
-    }
+  if (email === "") {
+    alert("Please enter your email");
+    return;
+  }
 
-    let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    if (!emailPattern.test(email)) {
-        alert("Please enter a valid email");
-        return;
-    }
-
-    if (message === "") {
-        alert("Please enter a message");
-        return;
-    }
-
-    alert("Form submitted successfully!");
-      document.getElementById("contactForm").reset();
+  if (!emailPattern.test(email)) {
+    alert("Please enter a valid email");
+    return;
+  }
+  if (message === "") {
+    alert("Please enter a message");
+    return;
+  }
+  alert("Form submitted successfully!");
+  document.getElementById("contactForm").reset();
 }
